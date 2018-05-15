b209e7426a038858d1f17d6ac354beaeb6d83f36
Steven <steven.tsui@nextmobile.hk>
2018-05-15 15:42:25 +0800

    fix bug

--- app/src/main/java/com/example/steven/stk/data/model/ResponseModel.kt
+++ app/src/main/java/com/example/steven/stk/data/model/ResponseModel.kt
@@ -2,4 +2,4 @@ package com.example.steven.stk.data.model
 
 import java.lang.reflect.Type
 
-data class ResponseModel(val state : String, val content : Type)
+data class ResponseModel(val state : String, val content : Any)

f216d372702506be904047664fac96dd56d52391
Steven <steven.tsui@nextmobile.hk>
2018-06-21 23:01:13 +0800

    add appbarlayout

--- app/src/main/java/com/example/steven/stk/activity/ArticleDetailActivity.kt
+++ app/src/main/java/com/example/steven/stk/activity/ArticleDetailActivity.kt
@@ -1,21 +1,27 @@
 package com.example.steven.stk.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import com.example.steven.stk.R
 import com.example.steven.stk.base.activity.BaseActivity
 import com.example.steven.stk.extension.plugActivityComponent
+import kotlinx.android.synthetic.main.activity_article_detail.*
 
 /**
  * Created by steven on 7/6/2018.
  */
 class ArticleDetailActivity: BaseActivity(){
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+//        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+
+        setContentView(R.layout.activity_article_detail)
 
         plugActivityComponent().inject(this)
+
+        toolbar.title = "STK"
+        toolbar.subtitle = "Testing Dagger2 with Kotlin"
+//        setSupportActionBar(toolbar)
     }
 }
--- app/src/main/java/com/example/steven/stk/custom/MyButtonBehavior.kt
+++ app/src/main/java/com/example/steven/stk/custom/MyButtonBehavior.kt
@@ -1,5 +1,36 @@
 package com.example.steven.stk.custom
 
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.CoordinatorLayout
+import android.view.View
+import android.widget.Button
+
+
 /**
  * Created by steven on 21/6/2018.
  */
+class MyButtonBehavior: CoordinatorLayout.Behavior<Button>(){
+
+    override fun layoutDependsOn(parent: CoordinatorLayout?, child: Button?, dependency: View?): Boolean {
+        return dependency == AppBarLayout::class
+    }
+
+    override fun onDependentViewChanged(parent: CoordinatorLayout?, child: Button?, dependency: View?): Boolean {
+
+
+        val top = dependency?.y
+
+        val layoutParams = child?.layoutParams as CoordinatorLayout.LayoutParams
+        layoutParams.topMargin = top as Int
+        child?.layoutParams = layoutParams
+
+        return true
+    }
+
+    private fun setPosition(v: View, x: Int, y: Int) {
+        val layoutParams = v.layoutParams as CoordinatorLayout.LayoutParams
+        layoutParams.leftMargin = x
+        layoutParams.topMargin = y
+        v.layoutParams = layoutParams
+    }
+}
